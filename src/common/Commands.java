01a76563ff13e534e3745f8f6329051341b9f818
emrey99 <yashiemre99@gmail.com>
2021-09-20 16:58:10 +0300

    All commands implemented in Engine

--- src/common/Commands.java
+++ src/common/Commands.java
@@ -6,7 +6,16 @@ import java.util.List;
 
 public class Commands {
 
-   List<String> commands = Arrays.asList("insertChild","insertFather","insertMother");
+   List<String> commands = Arrays.asList("insertChild", "insertFather", "insertMother",
+           "removeFather","removeMother","removeChild",
+           "insertGrandFatherOne","insertGrandFatherTwo","removeGrandFatherOne","removeGrandFatherTwo",
+           "insertGrandMotherOne","insertGrandMotherTwo",
+           "removeGrandMotherOne","removeGrandMotherTwo",
+           "viewAllChilds","viewAllMothers","viewAllFathers",
+           "viewAllGrandMotherOnes","viewAllGrandMotherTwos",
+           "viewAllGrandFatherOnes","viewAllGrandFatherTwos",
+           "viewEntireFamilyById");
+
 
 }
 
--- src/core/Engine.java
+++ src/core/Engine.java
@@ -25,10 +25,22 @@ public class Engine implements InterfaceEngine {
 
             String line = scanner.nextLine();
 
-            List<String> commands = Arrays.asList("insertChild", "insertFather", "insertMother","removeFather","removeMother","removeChild");
+//            Commands commands = new Commands();
+
+            List<String> commands = Arrays.asList("insertChild", "insertFather", "insertMother",
+                    "removeFather","removeMother","removeChild",
+                    "insertGrandFatherOne","insertGrandFatherTwo","removeGrandFatherOne","removeGrandFatherTwo",
+                    "insertGrandMotherOne","insertGrandMotherTwo",
+                    "removeGrandMotherOne","removeGrandMotherTwo",
+                    "viewAllChilds","viewAllMothers","viewAllFathers",
+                    "viewAllGrandMotherOnes","viewAllGrandMotherTwos",
+                    "viewAllGrandFatherOnes","viewAllGrandFatherTwos",
+                    "viewEntireFamilyById");
+
 
             if (line.equals("help")) {
                 for (String command : commands) {
+
                     System.out.println(command);
                 }
 
@@ -114,6 +126,8 @@ public class Engine implements InterfaceEngine {
 
                         controller.removeFather(idToRemove);
 
+                        System.out.println(FATHER_REMOVED_MESSAGE);
+
                     case  "removeMother":
 
                         System.out.println(MOTHER_REMOVE_REQUIREMENTS);
@@ -125,6 +139,8 @@ public class Engine implements InterfaceEngine {
                         int removeId = Integer.parseInt(line1[0]);
 
                         controller.removeMother(removeId);
+
+                        System.out.println(MOTHER_REMOVED_MESSAGE);
 
                     case  "removeChild":
 
@@ -138,8 +154,171 @@ public class Engine implements InterfaceEngine {
 
                         controller.removeChild(childl);
 
+                        System.out.println(CHILD_REMOVED_MESSAGE);
 
-                    default:
+                    case "insertGrandFatherOne":
+
+                        System.out.println(INSERT_FATHER_REQUIREMENTS_MESSAGE);
+
+                        line = scanner.nextLine();
+
+                        String[] tokens1 = line.split("\\s");
+
+                        int grandfatheoneId = Integer.parseInt(tokens1[0]);
+
+                        String grandFatherName = tokens1[1];
+
+                        String grandFatherCityName = tokens1[2];
+
+                        controller.insertGrandFatherOne(grandfatheoneId,grandFatherName,grandFatherCityName);
+
+                        System.out.println(GRAND_FATHER_ONE_ADDED);
+
+                    case "insertGrandFatherTwo":
+
+                        System.out.println(INSERT_FATHER_REQUIREMENTS_MESSAGE);
+
+                        line = scanner.nextLine();
+
+                        String[] tokens2 = line.split("\\s");
+
+                        int grandfathetwoId = Integer.parseInt(tokens2[0]);
+
+                        String grandFatherTwoName = tokens2[1];
+
+                        String grandFatherTwoCityName = tokens2[2];
+
+                        controller.insertGrandFatherOne(grandfathetwoId,grandFatherTwoName,grandFatherTwoCityName);
+
+                        System.out.println(GRAND_FATHER_TWO_ADDED);
+
+                    case  "insertGrandMotherOne":
+
+                        System.out.println(INSERT_MOTHER_REQUIREMENTS_MESSAGE);
+
+                        line = scanner.nextLine();
+
+                        String[] tokens3 = line.split("\\s");
+
+                        int grandmotherOneId = Integer.parseInt(tokens3[0]);
+
+                        String grandmotherOneName = tokens3[1];
+
+                        String grandMotherOneCityName = tokens3[2];
+
+                        controller.insertGrandFatherOne(grandmotherOneId,grandmotherOneName,grandMotherOneCityName);
+
+                        System.out.println(GRAND_MOTHER_ONE_ADDED);
+
+                    case  "insertGrandMotherTwo":
+
+                        System.out.println(INSERT_MOTHER_REQUIREMENTS_MESSAGE);
+
+                        line = scanner.nextLine();
+
+                        String[] tokens4 = line.split("\\s");
+
+                        int grandmotherTwoId = Integer.parseInt(tokens4[0]);
+
+                        String grandmotherTwoName = tokens4[1];
+
+                        String grandMotherTwoCityName = tokens4[2];
+
+                        controller.insertGrandFatherOne(grandmotherTwoId,grandmotherTwoName,grandMotherTwoCityName);
+
+                        System.out.println(GRAND_MOTHER_TWO_ADDED);
+
+
+                    case  "removeGrandMotherOne":
+
+                        System.out.println(MOTHER_REMOVE_REQUIREMENTS);
+
+                        line = scanner.nextLine();
+
+                        String[] line2 = line.split("\\s");
+
+                        int removeMotherId = Integer.parseInt(line2[0]);
+
+                        controller.removeGrandMotherOne(removeMotherId);
+
+                        System.out.println(GRAND_MOTHER_REMOVED_MESSAGE);
+
+                    case  "removeGrandMotherTwo":
+
+                        System.out.println(MOTHER_REMOVE_REQUIREMENTS);
+
+                        line = scanner.nextLine();
+
+                        String[] line3 = line.split("\\s");
+
+                        int removeMotherTwoId = Integer.parseInt(line3[0]);
+
+                        controller.removeGrandMotherTwo(removeMotherTwoId);
+
+                        System.out.println(GRAND_MOTHER_REMOVED_MESSAGE);
+
+                    case  "removeGrandFatherOne":
+
+                        System.out.println(FATHER_REMOVE_REQUIREMENTS);
+
+                        line = scanner.nextLine();
+
+                        String[] line4 = line.split("\\s");
+
+                        int removeGrandFarherOneId = Integer.parseInt(line4[0]);
+
+                        controller.removeGrandMotherOne(removeGrandFarherOneId);
+
+                        System.out.println(GRAND_FATHER_REMOVED_MESSAGE);
+
+                    case  "removeGrandFatherTwo":
+
+                        System.out.println(FATHER_REMOVE_REQUIREMENTS);
+
+                        line = scanner.nextLine();
+
+                        String[] line5 = line.split("\\s");
+
+                        int removeGrandFarherTwoId = Integer.parseInt(line5[0]);
+
+                        controller.removeGrandMotherTwo(removeGrandFarherTwoId);
+
+                        System.out.println(GRAND_FATHER_REMOVED_MESSAGE);
+
+                    case "viewAllChilds":
+
+                        controller.viewAllChilds();
+
+                    case "viewAllFathers":
+                        controller.viewAllFathers();
+
+                    case "viewAllMothers":
+                        controller.viewAllMothers();
+
+                    case "viewAllGrandMotherOnes":
+                        controller.viewAllGrandMotherOnes();
+
+                    case "viewAllGrandMotherTwos":
+                        controller.viewAllGrandMotherOnes();
+
+                    case "viewAllGrandFatherOnes":
+                        controller.viewAllGrandFatherOnes();
+
+                    case "viewAllGrandFatherTwos":
+                        controller.viewAllGrandFatherTwos();
+
+                    case "viewEntireFamilyById":
+
+                        line = scanner.nextLine();
+
+                        String[] line6 = line.split("\\s");
+
+                        int FamilyId = Integer.parseInt(line6[0]);
+
+                        controller.viewEntireFamilyById(FamilyId);
+
+
+                        default:
 
                         System.out.println(INVALID_INPUT);
 
